d29044be720449bfd63a425c7a85523cdd3b008f
Kian Jazayeri <95212993+kianz20@users.noreply.github.com>
2025-10-08 23:58:20 +1300

    Add dropdown menu to Navbar using react-menu
    Integrated @szhsin/react-menu for a dropdown in the Navbar, grouping 'Latest News', 'Reviews', and 'Brands' under an Articles hover menu. Updated dependencies and imports to support the new menu functionality.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,27 +1,46 @@
-import { Link } from "react-router-dom";
 import { Box, Button, Divider } from "@mui/material";
+import "@szhsin/react-menu/dist/index.css";
+import "@szhsin/react-menu/dist/transitions/zoom.css";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../styles/Navbar.module.css";
+import { ControlledMenu, MenuItem, useHover } from "@szhsin/react-menu";
+import { useRef, useState } from "react";
 
 const Navbar = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isOpen, setOpen] = useState(false);
+  const { anchorProps, hoverProps } = useHover(isOpen, setOpen);
+  const navigate = useNavigate();
+
   return (
     <Box>
       <Box display="flex" alignItems="center" justifyContent="space-between">
-        {/* Center buttons */}
         <Button component={Link} to="/" className={styles.link}>
           Home
         </Button>
-        <Button component={Link} to="/articles" className={styles.link}>
+
+        <Box
+          ref={ref}
+          {...anchorProps}
+          onClick={() => navigate("/articles")}
+          sx={{ cursor: "pointer" }}
+        >
           Articles
-        </Button>
-        <Button component={Link} to="/latestNews" className={styles.link}>
-          Latest News
-        </Button>
-        <Button component={Link} to="/reviews" className={styles.link}>
-          Reviews
-        </Button>
-        <Button component={Link} to="/brands" className={styles.link}>
-          Brands
-        </Button>
+        </Box>
+
+        <ControlledMenu
+          {...hoverProps}
+          anchorRef={ref as React.RefObject<Element>}
+          state={isOpen ? "open" : "closed"}
+          onClose={() => setOpen(false)}
+        >
+          <MenuItem onClick={() => navigate("/latestNews")}>
+            Latest News
+          </MenuItem>
+          <MenuItem onClick={() => navigate("/reviews")}>Reviews</MenuItem>
+          <MenuItem onClick={() => navigate("/brands")}>Brands</MenuItem>
+        </ControlledMenu>
+
         <Button component={Link} to="/about" className={styles.link}>
           About
         </Button>
